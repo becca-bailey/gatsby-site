4ac04e0d59caee5e1481296077def76051047745
Becca Nelson <beccanelson88@gmail.com>
2015-10-30 23:21:52 -0500

    Adds projects to projects directory

--- cheat-sheet.js
+++
@@ -1,7 +0,0 @@
-var html = document.getElementById("html");
-var htmlText = document.getElementByID("html_text");
-htmlText.style.display = "none";
-
-html.addEventListener("click", function() {
-	htmlText.style.display = "block";
-});
