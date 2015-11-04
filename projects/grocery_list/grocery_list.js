2cc52373c729d46e42463a0d6ef0034bca4043aa
Becca Nelson <beccanelson88@gmail.com>
2015-11-04 01:25:36 -0600

    Adds features for grocery lists

--- projects/grocery_list/grocery_list.js
+++ projects/grocery_list/grocery_list.js
@@ -9,14 +9,19 @@ var listItems = document.getElementsByTagName("input");
 var displayList = document.getElementById("display_list");
 var switchList = document.getElementById("switch_list");
 var removeList = document.getElementById("remove_list");
+var updateQuantity = document.getElementById("update_quantity");
+var printerFriendly = document.getElementById("printer_friendly");
+var listArea = document.getElementById("list_area");
 
 newItem.style.display = "none";
 removeItem.style.display = "none";
 displayList.style.display = "none";
 switchList.style.display = "none";
 removeList.style.display = "none";
+updateQuantity.style.display = "none";
+printerFriendly.style.display = "none";
 
-groceryLists = new Object();
+var groceryLists = new Object();
 
 var listItem = function() {
     this.item = item;
@@ -29,12 +34,25 @@ function printList(object) {
 		list.innerHTML += "<li><input type = 'checkbox' value = " + item + ">" + item + ": " + object[item] + "</input></li>"
 	}
    	removeItem.style.display = "block";
+   	updateQuantity.style.display = "block";
+}
+
+function printAll() {
+	list.innerHTML = "";
+	for (listName in groceryLists) {
+		listArea.innerHTML += "<h1>" + listName + "</h1>";
+		listArea.innerHTML += "<ul>";
+		for (item in groceryLists[listName]) {
+			listArea.innerHTML += "<li class='print'>" + item + ": " + groceryLists[listName][item] + "</li>"
+		}
+		listArea.innerHTML += "</ul>";
+	}
 }
 
 function listMenu(object) {
 	displayList.innerHTML = "";
 	for (item in object) {
-		displayList.innerHTML += "<option value = '" + item + "' >" + item + "</option>";
+		displayList.innerHTML = "<option value = '" + item + "' >" + item + "</option>" + displayList.innerHTML;
 	}
 }
 
@@ -43,6 +61,7 @@ function displayButtons() {
     displayList.style.display = "block";
     switchList.style.display = "block";
     removeList.style.display = "block";
+    printerFriendly.style.display = "block";
 }
 
 newList.addEventListener("click", function() {
@@ -69,6 +88,9 @@ removeItem.addEventListener("click", function() {
 		if (listItems[i].checked) {
 			delete groceryLists[listName][listItems[i].value];
 		}
+		else {
+			alert("Please check the item(s) you want to delete.")
+		}
 	}
 	printList(groceryLists[listName]);
 });
@@ -83,11 +105,34 @@ removeList.addEventListener("click", function() {
 	var listName = displayList.options[displayList.selectedIndex].value;
 	if(confirm("Are you sure you want to delete " + listName + "?")) {
 		delete groceryLists[listName];		
+		if (listTitle.innerHTML === listName) {
+			listMenu(groceryLists);
+			listTitle.innerHTML = "";
+			list.innerHTML = "";
+		}
 	}
-	listMenu(groceryLists);
-	if (listTitle.innerHTML === listName) {
-		listTitle.innerHTML = "";
-		list.innerHTML = "";
+});
+
+updateQuantity.addEventListener("click", function() {
+	var quantity = prompt("How much would you like to buy?");
+	var listName = displayList.options[displayList.selectedIndex].value;
+	for (var i = 0; i < listItems.length; i++) {
+		if (listItems[i].checked) {
+			groceryLists[listName][listItems[i].value] = quantity;
+		}
+		else {
+			alert("Please check the item(s) you want to update.")
+		}
 	}
+	printList(groceryLists[listName]);
+});
+
+printerFriendly.addEventListener("click", function() {
+	listArea.style.float = "none";
+	listArea.style.width = "100%";
+	listArea.style.backgroundColor = "white";
+	listTitle.style.display = "none";
+	document.getElementById("controls").style.display = "none";
+	printAll();
 });
 
